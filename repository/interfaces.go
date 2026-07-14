fa09e72cf0bdef832c90bc71690f2e55860bdb92
riobatubara <outrageryo@gmail.com>
2026-07-14 19:46:40 +0700

    refactor: add output return for CreateEstate

--- repository/interfaces.go
+++ repository/interfaces.go
@@ -5,7 +5,7 @@ import "context"
 //go:generate mockgen -source=interfaces.go -destination=interfaces.mock.gen.go -package=repository
 type RepositoryInterface interface {
 	GetTestById(ctx context.Context, input GetTestByIdInput) (output GetTestByIdOutput, err error)
-	CreateEstate(ctx context.Context, input CreateEstateInput) (err error)
+	CreateEstate(ctx context.Context, input CreateEstateInput) (output CreateEstateOutput, err error)
 	GetEstate(ctx context.Context) (output GetEstateOutput, err error)
 	GetEstateById(ctx context.Context, input GetEstateByIdInput) (output GetEstateByIdOutput, err error)
 	CreateTree(ctx context.Context, input CreateTreeInput) (err error)
